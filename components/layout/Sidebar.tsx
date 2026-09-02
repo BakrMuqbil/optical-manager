45995cdbe8bb8f17a2fe093ff27ad62d04a03939
BakrMuqbil <bakr.muqbil@gmail.com>
2026-09-02 08:51:11 +0300

    Fix multi-worker crash by limiting cpus to 1

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Home, FileText, ClipboardList, Settings, Menu, X, PlusCircle } from "lucide-react";
+import { Home, ClipboardList, Settings, Menu, X, PlusCircle } from "lucide-react";
 import { useState } from "react";
 
 const links = [
