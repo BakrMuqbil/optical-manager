f8d09f9ef6bc6527f86959bec4aa73c9c1641df8
BakrMuqbil <bakr.muqbil@gmail.com>
2026-09-02 07:08:04 +0300

    Fix multi-worker crash by limiting cpus to 1

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   serverExternalPackages: ["better-sqlite3"],
   typescript: {
-    // إتاحة البناء حتى لو توفرت تحذيرات غير مؤثره
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
   webpack: (config) => {
     config.cache = false;
